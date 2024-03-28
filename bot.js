488ec1fded2b300e8f3c1003278a400e42cb80b7
Tony Tran <ttcomm45@gmail.com>
2024-03-28 14:40:20 -0400

    intents updated

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
-const clientdiscord = new Discord.Client();
+
+const discordClient = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
 const token = process.env.DISCORD_BOT_TOKEN;
 
 const { Client } = require('pg');

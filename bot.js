0230195da4564dc64cf5c689ba66af8cdeb32824
Tony Tran <ttcomm45@gmail.com>
2024-03-29 22:54:42 -0400

    fix no token x2

--- bot.js
+++ bot.js
@@ -16,7 +16,6 @@ app.listen(PORT, () => {
 
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });

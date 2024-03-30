2c8d88ad8d63c7b7d7f13999b1ee59e10f9ce8c2
Tony Tran <ttcomm45@gmail.com>
2024-03-29 23:58:56 -0400

    connect to pg update

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { Client } = require('pg');
+
+const pgClient = new Client({
+    connectionString: process.env.POSTGRES_CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false,  // Necessary for Heroku
+    },
+});
+
+pgClient.connect();
 
 // Function to check if the "messages" table exists and create it if not
 async function ensureTableExists() {

bd8e364c5cf56e16661360d344b8fa41d0409e1d
Tony Tran <ttcomm45@gmail.com>
2024-03-31 23:07:07 -0400

    install string-sim and update add

--- commands/utility/add.js
+++ commands/utility/add.js
@@ -1,15 +1,19 @@
 // add.js within commands/utility folder
 const { SlashCommandBuilder } = require('discord.js');
 const { Client } = require('pg');
+const stringSimilarity = require('string-similarity');
 
 const pgClient = new Client({
   connectionString: process.env.POSTGRES_CONNECTION_STRING,
   ssl: {
-      rejectUnauthorized: false,  // Necessary for Heroku
+    rejectUnauthorized: false,  // Necessary for Heroku
   },
 });
+
 pgClient.connect();
 console.log("client connected (add)");
+
+const ingredientsList = Object.keys(ingredients).map(ingredient => ingredient.toLowerCase());
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -29,23 +33,32 @@ module.exports = {
         .setRequired(true)),
   async execute(interaction) {
     const tableName = interaction.options.getString('tablename');
-    const ingredient = interaction.options.getString('category');
+    const userCategory = interaction.options.getString('category');
     const value = interaction.options.getInteger('value');
-    
-    // Construct the SQL query to update the score for a given ingredient
+
+    const normalizedInput = userCategory.toLowerCase();
+    const matches = stringSimilarity.findBestMatch(normalizedInput, ingredientsList);
+
+    // Respond with an error message if no similar ingredient is found
+    if (matches.bestMatch.rating < 0.8) {  // Adjust this threshold as needed
+      await interaction.reply("Please enter a valid category.");
+      return;
+    }
+
+    const matchedIngredient = matches.bestMatch.target;
+
     const updateQuery = `
       UPDATE "${tableName}"
       SET score = score + $1
-      WHERE ingredient = $2;
+      WHERE LOWER(ingredient) = $2;
     `;
 
     try {
-      // Execute the query with value and ingredient as parameters
-      await pgClient.query(updateQuery, [value, ingredient]);
-      await interaction.reply(`Added ${value} to ${ingredient} in table ${tableName}.`);
+      await pgClient.query(updateQuery, [value, matchedIngredient]);
+      await interaction.reply(`Added ${value} to ${matchedIngredient} in table ${tableName}.`);
     } catch (error) {
       console.error('Error updating category:', error);
-      await interaction.reply(`Failed to update ${ingredient} in table ${tableName}.`);
+      await interaction.reply(`Failed to update ${matchedIngredient} in table ${tableName}.`);
     }
   },
 };

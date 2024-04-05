f6af3cde344f31bb9e5e0915335700afa0247a24
Tony Tran <ttcomm45@gmail.com>
2024-04-05 16:29:58 -0400

    change path

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const pgClient = require('../../db');
+const pgClient = require('commands/utility/db');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const token = process.env.DISCORD_BOT_TOKEN;
